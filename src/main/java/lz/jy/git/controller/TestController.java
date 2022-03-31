51e11ce49b1e09e100c62282409e161a00c035e9
sumengyang <Sumy_1121@163.com>
2022-03-31 18:14:46 +0800

    这是dev分支第一次提交

--- src/main/java/lz/jy/git/controller/TestController.java
+++ src/main/java/lz/jy/git/controller/TestController.java
@@ -3,5 +3,6 @@ package lz.jy.git.controller;
 public class TestController {
     public static void main(String[] args){
         System.out.println("hello git!");
+        System.out.println("hello git dev");
     }
 }
